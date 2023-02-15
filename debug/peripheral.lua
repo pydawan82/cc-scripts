a0c4922c545ddf9b30e096a7576ee641cf9d8fb8
David Birtles <dbirtles@hotmail.com>
2023-02-15 11:43:23 +0100

    reactor display fix

--- debug/peripheral.lua
+++ debug/peripheral.lua
@@ -12,9 +12,9 @@ t_fuel_tank = 0
 
 function p.fuelTank()
     return {
-        level = function()
+        fuel = function()
             t_fuel_tank = t_fuel_tank + 1
-            return (math.sin(t_fuel_tank/6)+1) * 5000
+            return (math.sin(t_fuel_tank / 6) + 1) * 5000
         end,
         capacity = function()
             return 10000
@@ -27,7 +27,7 @@ function p.getControlRod(index)
     return {
         level = function()
             t_control_rod = t_control_rod + 1
-            return (math.sin(t_control_rod/6)+1) * 50
+            return (math.sin(t_control_rod / 6) + 1) * 50
         end
     }
 end
@@ -35,7 +35,15 @@ end
 local t_fuel_temp = 0
 function p.fuelTemperature()
     t_fuel_temp = t_fuel_temp + 1
-    return (math.sin(t_fuel_temp/6)+1) * 2000
+    return (math.sin(t_fuel_temp / 6) + 1) * 2000
+end
+
+function p.coolTank()
+    return {
+        transitionedLastTick = function()
+            return 1000
+        end
+    }
 end
 
 return peripheral
--- scripts/reactor_display.lua
+++ scripts/reactor_display.lua
@@ -1,35 +1,38 @@
-require('printf')
-cmap = require('cmap')
+require('/lib/printf')
+cmap = require('/lib/cmap')
 
-reactor = peripheral.wrap('left')
-monitor = peripheral.wrap('right')
+reactor = peripheral.wrap('back')
+monitor = peripheral.wrap('bottom')
 
 local function clear()
     term.clear()
     term.setCursorPos(1, 1)
 end
 
-local function display(fuel_level, fuel_capcity, rod_level, temp)
+local function display(fuel_level, fuel_capcity, rod_level, temp, steam)
     term.setTextColor(cmap.colormap(fuel_level, fuel_capcity))
     printf('Fuel: %.1f/%.1fB', fuel_level, fuel_capcity)
 
     term.setTextColor(cmap.colormap(rod_level, 100))
     printf('Control Rod: %3d%%', math.floor(rod_level))
 
-    term.setTextColor(cmap.colormap(temp, 4000))
+    term.setTextColor(cmap.colormap(temp, 3500))
     printf('Temperature: %4dK', math.floor(temp))
+
+    term.setTextColor(cmap.colormap(steam, 80))
+    printf('Steam: %.1fB/t', steam)
 end
 
-
 local function main()
+    term.redirect(monitor)
     while true do
-        local fuel_level = reactor.fuelTank().level() / 1000
+        local fuel_level = reactor.fuelTank().fuel() / 1000
         local fuel_capacity = reactor.fuelTank().capacity() / 1000
         local rod_level = reactor.getControlRod(0).level()
         local temp = reactor.fuelTemperature()
-
+        local steam = reactor.coolantTank().transitionedLastTick() / 1000
         clear()
-        display(fuel_level, fuel_capacity, rod_level, temp)
+        display(fuel_level, fuel_capacity, rod_level, temp, steam)
 
         sleep(1)
     end
