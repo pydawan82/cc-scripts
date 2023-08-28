c09958a87b088080490aa0a945624e3f87d465ff
David Birtles <dbirtles@hotmail.com>
2023-08-28 20:29:27 +0200

    fixed called to peripheral

--- lib/peripherals.lua
+++ lib/peripherals.lua
@@ -4,14 +4,14 @@ peripherals = P
 local meta = {}
 
 function meta.__index(t, k)
-    t.__peripheral = t.__peripheral or peripherals.wrap(t.side)
+    t.__peripheral = t.__peripheral or peripheral.wrap(t.side)
     return t.__peripheral[k]
 end
 
 function P.wrap(side)
     local t = {
         __side = side,
-        __peripheral = peripherals.wrap(side)
+        __peripheral = peripheral.wrap(side)
     }
     setmetatable(t, meta)
 
