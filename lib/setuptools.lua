e3badd4da6c5c1b87819b6ff156ebcc2de94eaf3
David Birtles <dbirtles@hotmail.com>
2023-08-28 20:53:30 +0200

    fixed name

--- lib/setuptools.lua
+++ lib/setuptools.lua
@@ -4,13 +4,13 @@ setuptools = P
 function P.setup(scripts, libs, startup)
     fs.makeDir('lib')
     for _, lib in ipairs(libs) do
-        libname = lib .. '.lua'
+        lib = lib .. '.lua'
         fs.copy('disk/lib/' .. lib, 'lib/' .. lib)
     end
 
     fs.makeDir('scripts')
     for _, script in ipairs(scripts) do
-        scriptname = script .. '.lua'
+        script = script .. '.lua'
         fs.copy('disk/scripts/' .. script, 'scripts/' .. script)
     end
 
