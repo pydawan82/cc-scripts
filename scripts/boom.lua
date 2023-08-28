84d80149f24f56cbcb4111dbea6fb3d062f55c38
David Birtles <dbirtles@hotmail.com>
2023-08-28 20:12:45 +0200

    Added warning

--- scripts/boom.lua
+++ scripts/boom.lua
@@ -1,3 +1,10 @@
+print("BOOM! This will delete all files on the computer. [y/n]")
+local response = read()
+if response:subst(1, 1) ~= "y" then
+    print("Aborting")
+    return
+end
+
 files = fs.list("/")
 for _, file in ipairs(files) do
     if file == "rom" or file == "disk" then
