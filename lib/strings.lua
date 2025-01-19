c97910cd5defb7162ee2d8277d14facfe0967302
David Birtles <dbirtles@hotmail.com>
2025-01-19 14:21:00 +0100

    added split function

--- lib/strings.lua
+++ lib/strings.lua
@@ -25,3 +25,31 @@ function P.tobuffer(str)
 
     return buffer
 end
+
+---
+---@param str string
+---@param split string
+---@param n? integer
+function P.split(str, split, n)
+    splits = {}
+    start = 0
+
+    while (n == nil or n > 0) and str do
+        stop = str:find(split, start)
+        
+        if stop == nil then
+            break
+        end
+        
+        table.insert(splits, str:sub(start, stop - 1));
+        start = stop + 1
+
+        if n then n = n + 1 end
+    end
+
+    table.insert(splits, str:sub(start));
+
+    return splits
+end
+
+return P
